434bb1a6850166a75ae34cc7d80df6f6c0374953
Khai Tran <trankhai1478@gmail.com>
2024-03-21 10:58:45 +0700

    model-sequeclize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const cors = require('cors');
+const dbconn = require("./config/dbconn");
 const app = express();
 
 app.use(cors({
@@ -9,6 +10,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+dbconn();
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log("server runing 5000 ", +PORT);
